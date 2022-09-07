d55aec219e090a8468396314382f1e7748ecff1a
Santiago Mojica <samojica08@gmail.com>
2022-09-06 19:03:54 -0500

    Extraer dinero Js

---
+++ js/index.js
@@ -0,0 +1,75 @@
+//Declarar variables
+let nombreUsuario = "Santiago Mojica";
+let saldoCuenta = 50000;
+let limiteExtraccion = 500;
+let claveCorrecta = 0000;
+
+function iniciarSesion() {
+    let clave = prompt("Digite su clave");
+    if (clave == claveCorrecta) {
+        alert("Bienvenido " + nombreUsuario);
+        document.body.style.display = "block";
+        window.onload = function () {
+            cargarNombreEnPantalla();
+            saldo();
+            limite();
+
+        }
+    } else {
+        saldoCuenta = 50000;
+        alert("Su saldo se ha actualizado a cero por seguridad");
+    }
+}
+
+iniciarSesion();
+
+//Funciones para actualizar el valor de las variables HTML
+function cargarNombreEnPantalla() {
+    document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
+}
+function saldo() {
+    document.getElementById("saldo-cuenta").innerHTML = "$ " + saldoCuenta;
+}
+function limite() {
+    document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
+}
+
+//Llamado de funciones
+document.getElementById("extraer").onclick = function () {
+    ExtraerDinero();
+}
+document.getElementById("depositar").onclick = function () {
+    depositarServicio();
+}
+document.getElementById("pagar").onclick = function () {
+    cambiarLimiteDeExtraccion();
+}
+document.getElementById("transferir").onclick = function () {
+    cambiarLimiteDeExtraccion();
+}
+document.getElementById("limite").onclick = function () {
+    cambiarLimiteDeExtraccion();
+}
+
+
+
+//Funciones de back
+function ExtraerDinero() {
+    let ExtraerDinero = prompt("Cuanto dinero desea extraer? ");
+    if (ExtraerDinero > limiteExtraccion) {
+        alert("El monto a retirar supera el limite $"+limiteExtraccion);
+    } else {
+        let saldoActual = saldoCuenta - ExtraerDinero
+
+        saldoCuenta = saldoActual;
+
+        alert("Su extraccion exitosa");
+        actualizarSaldo();
+    }
+}
+
+function actualizarSaldo() {
+    document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
+}
+
+
